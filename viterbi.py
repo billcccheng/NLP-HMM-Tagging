1afcaca94e7ba44eee1444562cd309a87acf8ef4
Bill <iamkg05@gmail.com>
2016-10-08 15:03:26 -0500

    added function: get word set in sentence

--- viterbi.py
+++ viterbi.py
@@ -1,12 +1,19 @@
 import sys
 
-def read_file(file_name):
-  lines = [line.rstrip('\n') for line in open(file_name)]
-  return lines
+def get_sentence(file_name):
+  word_set = set()
+  with open(file_name) as f:
+    for line in f:
+      for word in line.split():
+        word_set.add(word)
+  return word_set
+  # print word_set
+  # lines = [line.rstrip('\n') for line in open(file_name)]
+  # return lines
 
 def main():
   file_name = sys.argv[1]
-  print read_file(file_name)
+  print list(get_sentence(file_name))
     
 if __name__ == "__main__":
     main()
